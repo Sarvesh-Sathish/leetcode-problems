b24f44809c5769f571f56e370e56f3090f5312b7
Sarvesh-Sathish <sarveshsathish@gatech.edu>
2024-03-18 18:26:34 -0500

    finished rotate array problem

---
+++ problems-solved/189-rotate-array.py
@@ -0,0 +1,15 @@
+def rotate(self, nums: List[int], k: int) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    k = k % len(nums)
+    lastElem = []
+    for i in range(len(nums) - k, len(nums)):
+        lastElem.append(nums[i])
+
+    lastIndex = len(nums) - 1
+    for i in range(lastIndex, 0, -1):
+        nums[i] = nums[i - k]
+
+    for i in range(0, len(lastElem)):
+        nums[i] = lastElem[i]
